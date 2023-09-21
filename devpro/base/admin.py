7b5c185126e2dff85956eb9bf06fbf96f40455f9
renzo <renzon@gmail.com>
2023-09-20 21:47:01 -0300

    Criado modelo para interagir com tabela legada

--- devpro/base/admin.py
+++ devpro/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from devpro.base.models import Curso
+
+
 # Register your models here.
+
+@admin.register(Curso)
+class CursosAdmin(admin.ModelAdmin):
+    list_display = 'id name created_at updated_at'.split()
--- devpro/base/models.py
+++ devpro/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cursos'
+
